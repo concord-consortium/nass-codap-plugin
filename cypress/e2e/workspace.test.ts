c252709857f1fa91cc10c7b38165ee6d17f9e938
Ethan McElroy <emcelroy@concord.org>
2025-10-30 11:23:58 -0400

    chore: add proxy env var to ci.yml, update tests

--- cypress/e2e/workspace.test.ts
+++ cypress/e2e/workspace.test.ts
@@ -3,9 +3,18 @@ context("Test the overall app", () => {
     cy.visit("");
   });
 
-  // describe("Desktop functionalities", () => {
-  //   it("renders with text", () => {
-  //     cy.get(".app").should("have.text", "Hello World");
-  //   });
-  // });
+  describe("App loads properly", () => {
+    it("renders with descriptive text", () => {
+      cy.get(".app-sectionHeaderText").should("have.text", "Retrieve data on U.S. agricultural statistics at the state or county level.");
+    });
+    it("renders with three dropdown sections: Place, Attributes, and Years", () => {
+      cy.get(".dropdown-dropdown").should("have.length", 3);
+      cy.get(".dropdown-dropdown").eq(0).contains("Place");
+      cy.get(".dropdown-dropdown").eq(1).contains("Attributes");
+      cy.get(".dropdown-dropdown").eq(2).contains("Years");
+    });
+    it("renders with a 'Get Data' button", () => {
+      cy.get("button").contains("Get Data");
+    });
+  });
 });
