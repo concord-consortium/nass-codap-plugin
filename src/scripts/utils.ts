a6cf16afa11d87cd52ef1a619a69be01fe48761e
Ethan McElroy <emcelroy@concord.org>
2025-11-04 13:24:45 -0500

    feat: add Clear Data Selection button (NP-11)

--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -1,4 +1,5 @@
 import { queryData } from "../constants/queryHeaders";
+import { IStateOptions } from "../constants/types";
 
 export const flatten = (arr: any[]): any[] => {
   return arr.reduce((acc: any[], val: any) =>
@@ -7,4 +8,18 @@ export const flatten = (arr: any[]): any[] => {
 
 export const getQueryParams = (attribute: string) => {
   return queryData.find((d) => d.plugInAttribute === attribute);
+};
+
+export const isDefaultSelection = (selectedOptions: IStateOptions, defaultOptions: IStateOptions): boolean => {
+  return Object.keys(defaultOptions).every((key) => {
+    const selected = selectedOptions[key as keyof IStateOptions];
+    const defaultVal = defaultOptions[key as keyof IStateOptions];
+
+    if (Array.isArray(selected) && Array.isArray(defaultVal)) {
+      return selected.length === defaultVal.length && 
+             selected.every((item, index) => item === defaultVal[index]);
+    }
+
+    return selected === defaultVal;
+  });
 };
